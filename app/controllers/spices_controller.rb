b9fe7a09b4ae9860401b41ef879182fef94ea372
Zach Underwood <zawood11@gmail.com>
2022-04-17 13:52:00 -0400

    completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,30 @@
+class SpicesController < ApplicationController
+    
+    def index
+        render json: Spice.all
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+end
